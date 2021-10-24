3d209bb73fae62d4431b00e105c9b0a9c1d03d3e
Alessandra <alereis95@gmail.com>
2021-10-23 22:51:36 -0300

    add paths com retorno de operaçoes matematicas

--- core/views.py
+++ core/views.py
@@ -3,3 +3,15 @@ from django.shortcuts import render, HttpResponse
 # Create your views here.
 def hello(request, name, age):
     return HttpResponse(f'<h1>Hello {name} de {age} anos.<h1>')
+
+def operacao(request, operacao, n1, n2):
+    if operacao == 'soma':
+        return HttpResponse(f'A soma de {n1} + {n2} é igual a {n1+n2}.')
+    elif operacao == 'subtração':
+        return HttpResponse(f'A subtração de {n1} - {n2} é igual a {n1-n2}.')
+    elif operacao == 'multiplicação':
+        return HttpResponse(f'A multiplicação de {n1} * {n2} é igual a {n1*n2}.')
+    elif operacao == 'divisão':
+        return HttpResponse(f'A divisão de {n1} / {n2} é igual a {n1/n2}.')
+    else:
+        return HttpResponse('Operação não compreendida. Por favor, digite a operação desejada corretamente, e no seguinte formato: divisão/número1/número2/')
--- hello_django/urls.py
+++ hello_django/urls.py
@@ -20,4 +20,5 @@ from core import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('hello/<name>/<int:age>', views.hello),
+    path('<operacao>/<int:n1>/<int:n2>', views.operacao),
 ]
