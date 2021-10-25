12298e3d566927fe7e3209f5ff83b8c0b650ff2e
Alessandra <alereis95@gmail.com>
2021-10-24 22:55:01 -0300

    criada tela de listagem de eventos

--- core/views.py
+++ core/views.py
@@ -1,17 +1,10 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import redirect, render, HttpResponse
+
+from core.models import Evento
 
 # Create your views here.
-def hello(request, name, age):
-    return HttpResponse(f'<h1>Hello {name} de {age} anos.<h1>')
-
-def operacao(request, operacao, n1, n2):
-    if operacao == 'soma':
-        return HttpResponse(f'A soma de {n1} + {n2} é igual a {n1+n2}.')
-    elif operacao == 'subtração':
-        return HttpResponse(f'A subtração de {n1} - {n2} é igual a {n1-n2}.')
-    elif operacao == 'multiplicação':
-        return HttpResponse(f'A multiplicação de {n1} * {n2} é igual a {n1*n2}.')
-    elif operacao == 'divisão':
-        return HttpResponse(f'A divisão de {n1} / {n2} é igual a {n1/n2}.')
-    else:
-        return HttpResponse('Operação não compreendida. Por favor, digite a operação desejada corretamente, e no seguinte formato: divisão/número1/número2/')
+def lista_eventos(request):
+    usuario = request.user
+    evento = Evento.objects.all()
+    dados = {'eventos': evento}
+    return render(request, 'agenda.html', dados)
--- hello_django/urls.py
+++ hello_django/urls.py
@@ -13,12 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls import url
 from django.contrib import admin
 from django.urls import path
+from django.views.generic import RedirectView
 from core import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('hello/<name>/<int:age>', views.hello),
-    path('<operacao>/<int:n1>/<int:n2>', views.operacao),
+    path('agenda/', views.lista_eventos),
+    path('', RedirectView.as_view(url='agenda/'))
 ]
